dc5265099090c58dbab7261b334af4c658b5f489
haliacmon <tuna271@gmail.com>
2025-03-05 22:40:25 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ function DemoMeetingTab(props: { label: string }) {
   };
   return (
     <div className={styles.tabContent}>
-      <p style={{ margin: 0 }}>Try LiveKit Meet for free with our live demo project.</p>
+      {/* <p style={{ margin: 0 }}>Try LiveKit Meet for free with our live demo project.</p> */}
       <button style={{ marginTop: '1rem' }} className="lk-button" onClick={startMeeting}>
         Start Meeting
       </button>
@@ -165,22 +165,14 @@ export default function Page() {
     <>
       <main className={styles.main} data-lk-theme="default">
         <div className="header">
-          <img src="/images/livekit-meet-home.svg" alt="LiveKit Meet" width="360" height="45" />
-          <h2>
-            Open source video conferencing app built on{' '}
-            <a href="https://github.com/livekit/components-js?ref=meet" rel="noopener">
-              LiveKit&nbsp;Components
-            </a>
-            ,{' '}
-            <a href="https://livekit.io/cloud?ref=meet" rel="noopener">
-              LiveKit&nbsp;Cloud
-            </a>{' '}
-            and Next.js.
+          {/* <img src="/images/livekit-meet-home.svg" alt="LiveKit Meet" width="360" height="45" /> */}
+          <h2 style={{ fontSize: '2.5rem', fontWeight: 'bold', margin: '1rem 0' }}>
+            Azakord
           </h2>
         </div>
         <Suspense fallback="Loading">
           <Tabs>
-            <DemoMeetingTab label="Demo" />
+            <DemoMeetingTab label="Room" />
             <CustomConnectionTab label="Custom" />
           </Tabs>
         </Suspense>
